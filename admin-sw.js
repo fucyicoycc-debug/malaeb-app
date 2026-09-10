7c6c683ebe44f1d2e44ebb0846c087791abc3e4c
fucyicoycc-debug <fucyicoycc@gmail.com>
2026-09-10 14:26:33 +0200

    Add files via upload

---
+++ admin-sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'shoot-admin-v2';
+const SHELL_FILES = [
+  './admin.html',
+  './admin-manifest.webmanifest',
+  './admin-icon-192.png',
+  './admin-icon-512.png',
+  './whistle.mp3'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone)).catch(()=>{});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
